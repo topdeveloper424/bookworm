7ec0b4e7ac1bdfac0c7dc37b05b1253ab9d332d3
bundler --v <mingOK424@hotmail.com>
2018-11-11 05:16:03 +0800

    message

---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+export default{
+    user:{
+        login:credentials=>
+        axios.post("api/auth",{credentials}).then(res => res.data.user)
+    }
+}
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import LoginForm from "../forms/LoginForm"
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import LoginForm from "../forms/LoginForm";
+import {login} from "../../actions/auth";
 
 class LoginPage extends Component {
 
-  submit = data=>{
-    console.log(data);
-  }
-  render() {
+    submit = data=>
+      this.props.login(data).then(()=>this.props.history.push("/"));
+
+      render() {
     return (<div>
       <h1>Login Page</h1>
       <LoginForm submit={this.submit} />
@@ -14,4 +17,11 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push:PropTypes.func.isRequired 
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null,{login})(LoginPage);
